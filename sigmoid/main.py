1fd420e5115a8122ca0a678eead614be41eebbd3
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2025-08-05 18:59:52 +0800

    organize to modules

--- sigmoid/main.py
+++ sigmoid/main.py
@@ -4,29 +4,32 @@ from os.path import join
 import matplotlib.pyplot as plt
 import numpy as np
 
-from mnist_loader import MnistDataloader
-from neural_network import NeuralNetwork
+from mnist_dataset.mnist_loader import MnistDataloader
+from sigmoid.neural_network import NeuralNetwork
 
-input_dir_path = "./input"
+input_dir_path = "./mnist_dataset"
 training_images_filepath = join(input_dir_path, "train-images.idx3-ubyte")
 training_labels_filepath = join(input_dir_path, "train-labels.idx1-ubyte")
 test_images_filepath = join(input_dir_path, "t10k-images.idx3-ubyte")
 test_labels_filepath = join(input_dir_path, "t10k-labels.idx1-ubyte")
 
+mnist_dataloader = MnistDataloader(
+    training_images_filepath,
+    training_labels_filepath,
+    test_images_filepath,
+    test_labels_filepath,
+)
+
 
 def main():
-    mnist_dataloader = MnistDataloader(
-        training_images_filepath,
-        training_labels_filepath,
-        test_images_filepath,
-        test_labels_filepath,
-    )
-    training_data, test_data = mnist_dataloader.load_data(
+    full_training_data, test_data = mnist_dataloader.load_data(
         shape="flat", one_hot_encode=True
     )
 
-    validation_data = training_data[50000:]
-    training_data = training_data[:50000]
+    # mnist dataset has 60000 training images, use 50000 for training and 10000 for validation
+    training_data = full_training_data[:50000]
+    validation_data = full_training_data[50000:]
+
     nn = NeuralNetwork([784, 100, 10])
     print("Stochastic gradient descent...")
     nn.SGD(training_data, 30, 10, 3.0, test_data=validation_data)
